1c2e7b50235476c377be4968016184c0f8a947e6
Erin Keeffe <edkeeffe@gmail.com>
2014-09-19 22:16:51 -0700

    show header image only after its loaded

---
+++ source/javascripts/bg-image-load.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  // $.Velocity.RegisterUI('transition.zoomIn', {
+  //     defaultDuration: 1000,
+  //     calls: [
+  //       [{ translateZ: 0, scale: [ 1, 1.05 ], opacity: 1}, 1, { easing: 'ease' }]
+  //     ]
+  // });
+
+
+  $('.fade-bg-canvas').waitForImages(function() {
+      // All descendant images have loaded, now slide up.
+      $(this).addClass('fade-bg-canvas-loaded');
+  });
+});
