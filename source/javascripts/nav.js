24e840822902900b0c0df37d796696318fea6d46
Erin Keeffe <edkeeffe@gmail.com>
2014-12-12 20:14:54 -0800

    replace js doc ready with turbolinks equiv

--- source/javascripts/nav.js
+++ source/javascripts/nav.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).on("page:update", function() {
   var overlayNav = $('.cd-overlay-nav'),
     overlayContent = $('.cd-overlay-content'),
     toggleNav = $('.cd-nav-trigger'),
